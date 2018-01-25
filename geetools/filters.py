8d3193fb1fd2ff21345fa1f631c8e1cd7fc83ce5
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-01-25 11:32:24 -0300

    new module for custom filters

---
+++ geetools/filters.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+Set of custom filters for Google Earth Engine.
+"""
+
+import ee
+
+import ee.data
+if not ee.data._initialized: ee.Initialize()
+
+def date_range(range):
+    """ Filter by DateRange
+
+    :param range: date range
+    :type range: ee.DateRange
+    :return: the filter to apply to a collection
+    :rtype: ee.Filter
+    """
+    ini = range.start()
+    end = range.end()
+    return ee.Filter.date(ini, end)
---
+++ geetools/tests/test_filters.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+import unittest
+import ee
+from .. import tools
+ee.Initialize()
+
+class TestTools(unittest.TestCase):
+    def test_date_range(self):
+        pass
