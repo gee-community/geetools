5748488a8b7c6eb1b438238edf991e75de643919
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-05-02 14:05:58 -0300

    new dev version

--- geetools/__init__.py
+++ geetools/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.0.13dev"
+__version__ = "0.0.14dev"
 
 __title__ = "geetools"
 __summary__ = "A set of useful tools to use with Google Earth Engine Python" \
