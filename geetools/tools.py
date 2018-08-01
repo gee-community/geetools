cef1c9ec14b98169e5ce0bf0017df50c69bc3a0f
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-08-01 09:38:59 -0300

    new function to convert an ESRI shp to a FeatureCollection

--- geetools/tools.py
+++ geetools/tools.py
@@ -11,6 +11,7 @@ import os
 from collections import OrderedDict
 import json
 import multiprocessing
+import shapefile
 
 import ee
 from ee import serializer, deserializer
@@ -1546,5 +1547,60 @@ def recrusive_delete_asset(assetId):
         # delete empty collection and/or folder
         ee.data.deleteAsset(assetId)
 
-def kml2geometry(filename):
-    pass
+def get_projection(filename):
+    ''' Get EPSG from a shapefile using ogr
+
+    :param filename: an ESRI shapefile (.shp)
+    :type filename: str
+    '''
+    try:
+        from osgeo import ogr
+    except:
+        import ogr
+
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    dataset = driver.Open(filename)
+
+    # from Layer
+    layer = dataset.GetLayer()
+    spatialRef = layer.GetSpatialRef()
+
+    return spatialRef.GetAttrValue("AUTHORITY", 1)
+
+def shp2ft(filename):
+    ''' Convert an ESRI file (.shp and .dbf must be present) to a
+    ee.FeatureCollection
+
+    :param filename: the name of the filename. If the shape is not in the
+        same path than the script, specify a path instead.
+    :type filename: str
+    :return: the FeatureCollection
+    :rtype: ee.FeatureCollection
+    '''
+    wgs84 = ee.Projection('EPSG:4326')
+    # read the filename
+    reader = shapefile.Reader(filename)
+    fields = reader.fields[1:]
+    field_names = [field[0] for field in fields]
+    field_types = [field[1] for field in fields]
+    types = dict(zip(field_names, field_types))
+    features = []
+    for sr in reader.shapeRecords():
+        # atr = dict(zip(field_names, sr.record))
+        atr = {}
+        for fld, rec in zip(field_names, sr.record):
+            fld_type = types[fld]
+            if fld_type == 'D':
+                value = ee.Date(rec.isoformat()).millis().getInfo()
+            elif fld_type in ['C', 'N', 'F']:
+                value = rec
+            else:
+                continue
+            atr[fld] = value
+        geom = sr.shape.__geo_interface__
+        geometry = ee.Geometry(geom, 'EPSG:' + get_projection(filename))\
+                     .transform(wgs84, 1)
+        feat = ee.Feature(geometry, atr)
+        features.append(feat)
+
+    return ee.FeatureCollection(features)
