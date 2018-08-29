ec509eb5d93713def45838a913d0e665cc1f4af1
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-08-29 11:49:08 -0300

    add some methods and initialize EE

--- geetools/tools_list.py
+++ geetools/tools_list.py
@@ -2,13 +2,19 @@
 """ Tools for Earth Engine ee.List objects """
 
 import ee
+import ee.data
+
+if not ee.data._initialized:
+    ee.Initialize()
 
 
-class List(object):
+class List(ee.ee_list.List):
     """ List class to hold ee.List methods """
 
-    @staticmethod
-    def replace_many(ee_list, replace):
+    def __init__(self, *args, **kwargs):
+        super(List, self).__init__(*args, **kwargs)
+
+    def replace_many(self, replace):
         """ Replace many elements of a Earth Engine List object
 
             **EXAMPLE**
@@ -22,25 +28,22 @@ class List(object):
 
         >> [1, "two", "three", "four"]
 
-        :param ee_list: list
-        :type ee_list: ee.List
         :param replace: values to replace
         :type replace: dict
         :return: list with replaced values
         :rtype: ee.List
         """
+        new = self
         for key, val in replace.items():
             if val:
-                ee_list = ee_list.replace(key, val)
-        return ee_list
+                new = new.replace(key, val)
+        return new
 
-    @staticmethod
-    def intersection(ee_list1, ee_list2):
+    def intersection(self, intersect):
         """ Find matching values. If ee_list1 has duplicated values that are
         present on ee_list2, all values from ee_list1 will apear in the result
 
-        :param ee_list1: one Earth Engine List
-        :param ee_list2: the other Earth Engine List
+        :param intersect: the other Earth Engine List
         :return: list with the intersection (matching values)
         :rtype: ee.List
         """
@@ -48,29 +51,46 @@ class List(object):
         def wrap(element, first):
             first = ee.List(first)
 
-            return ee.Algorithms.If(ee_list2.contains(element),
+            return ee.Algorithms.If(intersect.contains(element),
                                     first.add(element), first)
 
-        return ee.List(ee_list1.iterate(wrap, newlist))
+        return ee.List(self.iterate(wrap, newlist))
 
-    @staticmethod
-    def difference(ee_list1, ee_list2):
+    def difference(self, ee_list):
         """ Difference between two earth engine lists
 
-        :param ee_list1: one list
         :param ee_list2: the other list
         :return: list with the values of the difference
         :rtype: ee.List
         """
-        return ee_list1.removeAll(ee_list2).add(ee_list2.removeAll(ee_list1))\
-                       .flatten()
+        return self.removeAll(ee_list).add(ee_list.removeAll(self)).flatten()
 
-    @staticmethod
-    def remove_duplicates(ee_list):
+    def remove_duplicates(self):
         """ Remove duplicated values from a EE list object """
         newlist = ee.List([])
         def wrap(element, init):
             init = ee.List(init)
             contained = init.contains(element)
             return ee.Algorithms.If(contained, init, init.add(element))
-        return ee.List(ee_list.iterate(wrap, newlist))
+        return ee.List(self.iterate(wrap, newlist))
+
+    def get_from_dict(self, values):
+        """ Get a list of Dict's values from a list object. Keys must be unique
+    
+        :param values: dict to get the values for list's keys
+        :type values: ee.Dictionary
+        :return: a list of values
+        :rtype: ee.List
+        """
+        # self = ee.List(self) if isinstance(self, list) else self
+        values = ee.Dictionary(values) if isinstance(values, dict) else values
+
+        empty = ee.List([])
+
+        def wrap(el, first):
+            f = ee.List(first)
+            cond = values.contains(el)
+            return ee.Algorithms.If(cond, f.add(values.get(el)), f)
+
+        values = ee.List(self.iterate(wrap, empty))
+        return values
