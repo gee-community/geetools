596b2035ed607585e169f9c76d37164040ec4fe7
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-08-29 11:48:31 -0300

    add get_values method

--- geetools/tools_collection.py
+++ geetools/tools_collection.py
@@ -1,20 +1,27 @@
 # coding=utf-8
 """ Module holding tools for ee.ImageCollections and ee.FeatueCollections """
 import ee
+import ee.data
+
+if not ee.data._initialized:
+    ee.Initialize()
 
 
-class Collection(object):
+class Collection(ee.collection.Collection):
 
-    @staticmethod
-    def fill_with_last(collection):
-        """ Fill masked values of each image pixel with the last available value
+    def __init__(self, *args, **kwargs):
+        super(Collection, self).__init__(*args, **kwargs)
 
-        :param collection: the collection that holds the images that will be filled
-        :type collection: ee.ImageCollection
+    def fill_with_last(self):
+        """ Fill masked values of each image pixel with the last available
+        value
+
+        :param self: the self that holds the images that will be filled
+        :type self: ee.ImageCollection
         :rtype: ee.ImageCollection
         """
-        collection = collection.sort('system:time_start', True)
-        collist = collection.toList(collection.size())
+        new = self.sort('system:time_start', True)
+        collist = new.toList(new.size())
         first = ee.Image(collist.get(0)).unmask()
         rest = collist.slice(1)
 
@@ -41,3 +48,72 @@ class Collection(object):
 
         newcol = ee.List(rest.iterate(wrap, ee.List([first])))
         return ee.ImageCollection.fromImages(newcol)
+
+    def get_values(self, geometry, reducer=ee.Reducer.mean(), scale=None,
+                   id='system:index', properties=None, side='server'):
+        """ Return all values of all bands of an image collection in the
+            specified geometry
+
+        :param geometry: Point from where to get the info
+        :type geometry: ee.Geometry
+        :param scale: The scale to use in the reducer. It defaults to 10 due
+            to the minimum scale available in EE (Sentinel 10m)
+        :type scale: int
+        :param id: image property that will be the key in the result dict
+        :type id: str
+        :param properties: image properties that will be added to the resulting
+            dict
+        :type properties: list
+        :param side: 'server' or 'client' side
+        :type side: str
+        :return: Values of all bands in the ponit
+        :rtype: dict
+        """
+        if not scale:
+            # scale = minscale(ee.Image(self.first()))
+            scale = 1
+        else:
+            scale = int(scale)
+
+        propid = ee.Image(self.first()).get(id).getInfo()
+        def transform(eeobject):
+            if isinstance(propid, (int, float)):
+                return ee.Number(eeobject).format()
+            elif isinstance(propid, (str, unicode)):
+                return ee.String(eeobject)
+            else:
+                msg = 'property must be a number or string, found {}'
+                raise ValueError(msg.format(type(propid)))
+
+        if not properties:
+            properties = []
+        properties = ee.List(properties)
+
+        def listval(img, it):
+            theid = ee.String(transform(img.get(id)))
+            values = img.reduceRegion(reducer, geometry, scale)
+            values = ee.Dictionary(values)
+            img_props = img.propertyNames()
+
+            def add_properties(prop, ini):
+                ini = ee.Dictionary(ini)
+                condition = img_props.contains(prop)
+                def true():
+                    value = img.get(prop)
+                    return ini.set(prop, value)
+                # value = img.get(prop)
+                # return ini.set(prop, value)
+                return ee.Algorithms.If(condition, true(), ini)
+
+            with_prop = ee.Dictionary(properties.iterate(add_properties, values))
+            return ee.Dictionary(it).set(theid, with_prop)
+
+        result = self.iterate(listval, ee.Dictionary({}))
+        result = ee.Dictionary(result)
+
+        if side == 'server':
+            return result
+        elif side == 'client':
+            return result.getInfo()
+        else:
+            raise ValueError("side parameter must be 'server' or 'client'")
