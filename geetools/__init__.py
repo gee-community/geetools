4d8d51339af957dc228f59a5bbd34efac55d787d
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-08-30 11:27:02 -0300

    include modules when importing geetools directly

--- geetools/__init__.py
+++ geetools/__init__.py
@@ -3,6 +3,14 @@
 from __future__ import absolute_import, division, print_function
 from .tools import eprint
 
+from .tools_image import Mapping
+from .tools_collection import FeatureCollection
+from .tools_segmentation import SNIC
+
+from . import tools
+from . import tools_image, tools_list, tools_number, tools_dictionary,\
+              tools_collection, tools_segmentation
+
 __version__ = "0.0.21dev"
 
 __title__ = "geetools"
