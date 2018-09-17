9d23c09add1c97c5c23ab231ff22e77b3339d6a5
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-09-17 13:24:51 -0300

    new daterange_list function

--- geetools/tools/date.py
+++ geetools/tools/date.py
@@ -23,3 +23,42 @@ def millis2datetime(millis):
     seconds = millis/1000
     dt = timedelta(seconds=seconds)
     return EE_EPOCH + dt
+
+
+def daterange_list(start_date, end_date, interval=1, unit='month'):
+    """ Divide a range that goes from start_date to end_date into many
+        ee.DateRange, each one holding as many units as the interval.
+        For example, for a range from
+
+    :param start_date: the start date. For the second DateRange and the
+        following, it'll be one second after the end of the previus DateRange
+    :param end_date: the end date
+    :param interval: range of the DateRange in units
+    :param unit: can be 'year', 'month' 'week', 'day', 'hour', 'minute',
+        or 'second'
+    :return: a list holding ee.DateRange
+    :rtype: list
+    """
+    units = ['year', 'month' 'week', 'day', 'hour', 'minute', 'second']
+    if unit not in units:
+        raise ValueError('unit param must be one of {}'.format(units))
+
+    def callback(interval, unit):
+        def wrap(n, ini):
+            ini = ee.List(ini)
+            last_range = ee.DateRange(ini.get(-1))
+            last_date = last_range.end()
+            next_date = last_date.advance(1, 'second')
+            next_interval = next_date.advance(interval, unit)
+            return ini.add(ee.DateRange(next_date, next_interval))
+        return wrap
+
+    total_days = end_date.difference(start_date, unit)
+    total_dateranges = total_days.divide(interval).toInt()
+    dateranges_list = ee.List.sequence(1, total_dateranges)
+
+    # first daterange
+    first = ee.DateRange(start_date, start_date.advance(interval, unit))
+
+    return ee.List(dateranges_list.iterate(callback(interval, unit),
+                                           ee.List([first])))
