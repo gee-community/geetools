0c56936a0d690d73c4536f3d9cd30d8bb05eebe6
Rodrigo Principe <fitoprincipe82@gmail.com>
2018-09-21 10:41:10 -0300

    new `fromGeoJSON` function

--- geetools/tools/featurecollection.py
+++ geetools/tools/featurecollection.py
@@ -3,9 +3,21 @@
 import ee
 import ee.data
 import shapefile
+import json
 
 if not ee.data._initialized:
     ee.Initialize()
+
+GEOMETRY_TYPES = {
+    'LineString': ee.geometry.Geometry.LineString,
+    'LineRing': ee.geometry.Geometry.LinearRing,
+    'MultiLineString': ee.geometry.Geometry.MultiLineString,
+    'MultiPolygon': ee.geometry.Geometry.MultiPolygon,
+    'MultiPoint': ee.geometry.Geometry.MultiPoint,
+    'Point': ee.geometry.Geometry.Point,
+    'Polygon': ee.geometry.Geometry.Polygon,
+    'Rectangle': ee.geometry.Geometry.Rectangle,
+}
 
 
 def get_projection(filename):
@@ -67,5 +79,47 @@ def fromShapefile(filename):
             .transform(wgs84, 1)
         feat = ee.Feature(geometry, atr)
         features.append(feat)
+
+    return ee.FeatureCollection(features)
+
+
+def fromGeoJSON(filename, crs=None):
+    """ Create a FeatureCollection from a GeoJSON file
+
+    :param filename:
+    :type filename: str
+    :param crs: a coordinate reference system in EPSG format. If not specified
+        it will try to get it from the geoJSON, and if not there it will rise
+        an error
+    :type: crs: str
+    :return:
+    """
+    with open(filename, 'r') as geojson:
+        content = geojson.read()
+        geodict = json.loads(content)
+        features = []
+        # Get crs from GeoJSON
+        if not crs:
+            filecrs = geodict.get('crs').get('properties').get('name')
+            splitcrs = filecrs.split(':')
+            cleancrs = [part for part in splitcrs if part]
+            try:
+                if cleancrs[-1] == 'CRS84':
+                    crs = 'EPSG:4326'
+                elif cleancrs[-2] == 'EPSG':
+                    crs = '{}:{}'.format(cleancrs[-2], cleancrs[-1])
+                else:
+                    raise ValueError('{} not recognized'.format(filecrs))
+            except IndexError:
+                raise ValueError('{} not recognized'.format(filecrs))
+
+        for n, feat in enumerate(geodict.get('features')):
+            properties = feat.get('properties')
+            geom = feat.get('geometry')
+            ty = geom.get('type')
+            coords = geom.get('coordinates')
+            ee_geom = GEOMETRY_TYPES.get(ty)(coords, proj=ee.Projection(crs))
+            ee_feat = ee.feature.Feature(ee_geom, properties)
+            features.append(ee_feat)
 
     return ee.FeatureCollection(features)
