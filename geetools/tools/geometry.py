d8c164243ac216f8dc972967402185edfcc58c97
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-10-03 17:14:38 -0300

    get region from footprint

--- geetools/tools/geometry.py
+++ geetools/tools/geometry.py
@@ -7,6 +7,15 @@ if not ee.data._initialized:
     ee.Initialize()
 
 
+UNBOUNDED = [[[-180.0, -90.0], [180.0, -90.0],
+              [180.0, 90.0], [-180.0, 90.0],
+              [-180.0, -90.0]]]
+
+UNBOUNDED_2 = [[[None, None], [None, None],
+                [None, None], [None, None],
+                [None, None]]]
+
+
 def getRegion(eeobject, bounds=False):
     """ Gets the region of a given geometry to use in exporting tasks. The
     argument can be a Geometry, Feature or Image
@@ -18,15 +27,26 @@ def getRegion(eeobject, bounds=False):
     """
     if isinstance(eeobject, ee.Geometry):
         eeobject = eeobject.bounds() if bounds else eeobject
-        region = eeobject.getInfo()["coordinates"]
+        info = eeobject.getInfo()
+        region = info.get("coordinates")
     elif isinstance(eeobject, (ee.Feature, ee.Image,
                                ee.FeatureCollection, ee.ImageCollection)):
         eeobject = eeobject.geometry().bounds() if bounds else eeobject.geometry()
-        region = eeobject.getInfo()["coordinates"]
+        info = eeobject.getInfo()
+        region = info.get("coordinates")
+
+        # Check unbounded
+        if (region == UNBOUNDED or region == UNBOUNDED_2)\
+                and isinstance(eeobject, ee.Image):
+            footprint = info.get('system:footprint')
+            if footprint:
+                region = footprint.get('coordinates')
+
     elif isinstance(eeobject, list):
         condition = all([type(item) == list for item in eeobject])
         if condition:
             region = eeobject
     else:
         region = eeobject
+
     return region
