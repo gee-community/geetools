112832f57c09ad2865005ff16eefafb77495793b
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-10-19 11:07:48 -0300

    support GeometryCollections

--- geetools/tools/geometry.py
+++ geetools/tools/geometry.py
@@ -7,6 +7,15 @@ if not ee.data._initialized:
     ee.Initialize()
 
 
+UNBOUNDED = [[[-180.0, -90.0], [180.0, -90.0],
+              [180.0, 90.0], [-180.0, 90.0],
+              [-180.0, -90.0]]]
+
+UNBOUNDED_2 = [[[None, None], [None, None],
+                [None, None], [None, None],
+                [None, None]]]
+
+
 def getRegion(eeobject, bounds=False):
     """ Gets the region of a given geometry to use in exporting tasks. The
     argument can be a Geometry, Feature or Image
@@ -16,17 +25,33 @@ def getRegion(eeobject, bounds=False):
     :return: region coordinates ready to use in a client-side EE function
     :rtype: json
     """
+    def dispatch(geometry):
+        info = geometry.getInfo()
+        geomtype = info['type']
+        if geomtype == 'GeometryCollection':
+            geometries = info['geometries']
+            region = []
+            for geom in geometries:
+                this_type = geom['type']
+                if this_type in ['Polygon', 'MultiPolygon', 'Rectangle']:
+                    region.append(geom['coordinates'])
+        else:
+            region = info['coordinates']
+
+        return region
+
     if isinstance(eeobject, ee.Geometry):
-        eeobject = eeobject.bounds() if bounds else eeobject
-        region = eeobject.getInfo()["coordinates"]
+        geometry = eeobject.bounds() if bounds else eeobject
+        region = dispatch(geometry)
     elif isinstance(eeobject, (ee.Feature, ee.Image,
                                ee.FeatureCollection, ee.ImageCollection)):
-        eeobject = eeobject.geometry().bounds() if bounds else eeobject.geometry()
-        region = eeobject.getInfo()["coordinates"]
+        geometry = eeobject.geometry().bounds() if bounds else eeobject.geometry()
+        region = dispatch(geometry)
     elif isinstance(eeobject, list):
         condition = all([type(item) == list for item in eeobject])
         if condition:
             region = eeobject
     else:
         region = eeobject
+
     return region
