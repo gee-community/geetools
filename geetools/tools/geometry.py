fba8a33543ca7d42041708ff7b1985dba1f240e3
Rodrigo Principe <fitoprincipe82@gmail.com>
2018-12-05 14:10:39 -0300

    check for unbounded locally

--- geetools/tools/geometry.py
+++ geetools/tools/geometry.py
@@ -16,6 +16,21 @@ UNBOUNDED_2 = [[[None, None], [None, None],
                 [None, None]]]
 
 
+def isUnbounded(geometry):
+    """ Check if the geometry is unbounded using the module's definition of
+    unboundness. It's a client-side function. For server-side function use
+
+    `ee.Geometry.isUnbounded()`
+    """
+    bounds = geometry.getInfo()['coordinates']
+    check1 = bounds == UNBOUNDED
+    check2 = bounds == UNBOUNDED_2
+
+    if (check1 or check2):
+        return True
+    else:
+        return False
+
 def getRegion(eeobject, bounds=False):
     """ Gets the region of a given geometry to use in exporting tasks. The
     argument can be a Geometry, Feature or Image
@@ -40,13 +55,22 @@ def getRegion(eeobject, bounds=False):
 
         return region
 
+    # Geometry
     if isinstance(eeobject, ee.Geometry):
         geometry = eeobject.bounds() if bounds else eeobject
         region = dispatch(geometry)
-    elif isinstance(eeobject, (ee.Feature, ee.Image,
-                               ee.FeatureCollection, ee.ImageCollection)):
+    # Feature and Image
+    elif isinstance(eeobject, (ee.Feature, ee.Image)):
         geometry = eeobject.geometry().bounds() if bounds else eeobject.geometry()
         region = dispatch(geometry)
+    # FeatureCollection and ImageCollection
+    elif isinstance(eeobject, (ee.FeatureCollection, ee.ImageCollection)):
+        if bounds:
+            geometry = eeobject.geometry().bounds()
+        else:
+            geometry = eeobject.geometry().dissolve()
+        region = dispatch(geometry)
+    # List
     elif isinstance(eeobject, list):
         condition = all([type(item) == list for item in eeobject])
         if condition:
