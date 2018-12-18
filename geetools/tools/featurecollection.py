9b389e58fe73fde2a60eb558a4e6000960ca73dd
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-12-18 07:53:20 -0300

    function to add an unique ID to an Feature collection

--- geetools/tools/featurecollection.py
+++ geetools/tools/featurecollection.py
@@ -23,12 +23,45 @@ GEOMETRY_TYPES = {
 }
 
 
-def enumerateProperty(col, name='enumeration'):
-    """
+def addId(collection, name='id', start=1):
+    """ Add a unique numeric identifier, from parameter 'start' to
+    collection.size() stored in a property called with parameter 'name'
 
-    :param col:
-    :param name:
-    :return:
+    :param collection: the collection
+    :type collection: ee.FeatureCollection
+    :param name: the name of the resulting property
+    :type name: str
+    :param start: the number to start from
+    :type start: int
+    :return: the parsed collection with a new property
+    :rtype: ee.FeatureCollection
+    """
+    start = ee.Number(start)
+    collist = collection.toList(collection.size())
+    first = ee.Feature(collist.get(0))
+    rest = collist.slice(1)
+
+    # Set first id
+    first = ee.List([first.set(name, start)])
+
+    # Set rest
+    def over_col(feat, last):
+        last = ee.List(last)
+        last_feat = ee.Feature(last.get(-1))
+        feat = ee.Feature(feat)
+        last_id = ee.Number(last_feat.get('id'))
+        return last.add(feat.set('id', last_id.add(1)))
+
+    return ee.FeatureCollection(ee.List(rest.iterate(over_col, first)))
+
+
+def enumerateProperty(col, name='enumeration'):
+    """ Create a list of lists in which each element of the list is:
+    [index, element]. For example, if you parse a FeatureCollection with 3
+    Features you'll get: [[0, feat0], [1, feat1], [2, feat2]]
+
+    :param collection: the collection
+    :return: ee.FeatureCollection
     """
     enumerated = eecollection.enumerate(col)
 
