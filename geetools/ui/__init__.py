05db5c5efe0989647682d8144396edbd2de5ef58
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2018-12-27 16:13:52 -0300

    make use of new dispatcher module

--- geetools/ui/__init__.py
+++ geetools/ui/__init__.py
@@ -1,9 +1,12 @@
 # coding=utf-8
 """ User Interface Tools """
-from . import chart, imagestrip, ipymap, ipytools, maptool
 import ee
 import threading
-from .ipymap import Map
+import pprint
+from . import chart, imagestrip, ipymap, ipytools, maptool, dispatcher
+
+from IPython.display import display
+from ipywidgets import Output, HTML, VBox, Widget, Label, Accordion
 
 if not ee.data._initialized:
     ee.Initialize()
@@ -30,12 +33,7 @@ def eprint(*args, **kwargs):
     indent = kwargs.get('indent', 2)
     notebook = kwargs.get('notebook', NOTEBOOK)
     do_async = kwargs.get('do_async', ASYNC)
-
-    import pprint
     pp = pprint.PrettyPrinter(indent=indent)
-
-    from IPython.display import display
-    from ipywidgets import Output, HTML, VBox
 
     # VERTICAL GRID WIDGET TO OUTPUT RESULTS
     infowin = VBox([Output()]*len(args))
@@ -46,31 +44,6 @@ def eprint(*args, **kwargs):
         children[i] = val
         vbox.children = children
 
-    # DISPATCHER
-    def dispatch(eeobject):
-        module = getattr(eeobject, '__module__', None)
-        parent = module.split('.')[0] if module else None
-        if parent == ee.__name__:
-            # DISPATCH!!
-            if isinstance(eeobject, (ee.Date,)):
-                info = eeobject.format().getInfo()
-            else:
-                info = eeobject.getInfo()
-        else:
-            info = str(eeobject)
-
-        return info
-
-    # NOTEBOOK WIDGET
-    def get_widget(eeobject):
-        """ Create a widget with the eeobject information """
-        info = dispatch(eeobject)
-        # DISPATCH
-        if isinstance(info, (dict,)):
-            return ipytools.create_accordion(info)
-        else:
-            return HTML(str(info)+'<br/>')
-
     if do_async and not notebook:
         print('Cannot make async printing outside a Jupyter environment')
         do_async = False
@@ -80,10 +53,10 @@ def eprint(*args, **kwargs):
     def get_info(eeobject, index):
         """ Get Info """
         if notebook:
-            widget = get_widget(eeobject)
+            widget = dispatcher.widget_dispatcher(eeobject)
             setchildren(infowin, index, widget)
         else:
-            info_return[index] = dispatch(eeobject)
+            info_return[index] = dispatcher.eeobject_dispatcher(eeobject)
 
     for i, eeobject in enumerate(args):
         # DO THE SAME FOR EVERY OBJECT
@@ -131,3 +104,5 @@ def getInfo(eeobject):
         get_info(eeobject, False)
 
     return result
+
+from .ipymap import Map
--- geetools/ui/ipytools.py
+++ geetools/ui/ipytools.py
@@ -6,6 +6,7 @@ from ipywidgets import HTML, Tab, Accordion, Checkbox, HBox, Layout, Widget, \
 from traitlets import List, Unicode, observe, Instance, Tuple, Int, Float
 
 from .. import batch
+from .dispatcher import widget_dispatcher
 
 # imports for async widgets
 from threading import Thread
@@ -47,70 +48,7 @@ def create_object_output(object):
     ty = object.__class__.__name__
 
     if ty == 'Image':
-        info = object.getInfo()
-        image_id = info['id'] if 'id' in info else 'No Image ID'
-        prop = info.get('properties')
-        bands = info.get('bands')
-        bands_names = [band.get('id') for band in bands]
-
-        # BAND PRECISION
-        bands_precision = []
-        for band in bands:
-            data = band.get('data_type')
-            if data:
-                precision = data.get('precision')
-                bands_precision.append(precision)
-
-        # BAND CRS
-        bands_crs = []
-        for band in bands:
-            crs = band.get('crs')
-            bands_crs.append(crs)
-
-        # BAND MIN AND MAX
-        bands_min = []
-        for band in bands:
-            data = band.get('data_type')
-            if data:
-                bmin = data.get('min')
-                bands_min.append(bmin)
-
-        bands_max = []
-        for band in bands:
-            data = band.get('data_type')
-            if data:
-                bmax = data.get('max')
-                bands_max.append(bmax)
-
-        # BANDS
-        new_band_names = []
-        zipped_data = zip(bands_names, bands_precision, bands_min, bands_max,
-                          bands_crs)
-        for name, ty, mn, mx, epsg in zipped_data:
-            value = '<li><b>{}</b> ({}) {} to {} - {}</li>'.format(name,ty,
-                                                                   mn,mx,epsg)
-            new_band_names.append(value)
-        bands_wid = HTML('<ul>'+''.join(new_band_names)+'</ul>')
-
-        # PROPERTIES
-        if prop:
-            new_properties = []
-            for key, val in prop.items():
-                value = '<li><b>{}</b>: {}</li>'.format(key, val)
-                new_properties.append(value)
-            prop_wid = HTML('<ul>'+''.join(new_properties)+'</ul>')
-        else:
-            prop_wid = HTML('Image has no properties')
-
-        # ID
-        header = HTML('<b>Image id:</b> {id} </br>'.format(id=image_id))
-
-        acc = Accordion([bands_wid, prop_wid])
-        acc.set_title(0, 'Bands')
-        acc.set_title(1, 'Properties')
-        acc.selected_index = None # thisp will unselect all
-
-        return VBox([header, acc])
+        return widget_dispatcher(object)
     elif ty == 'FeatureCollection':
         try:
             info = object.getInfo()
