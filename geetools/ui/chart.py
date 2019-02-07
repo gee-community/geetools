b1084646f8ef84a76a1293ec94c2855570ee66bb
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2019-02-07 14:38:30 -0300

    fix issue https://github.com/gee-community/gee_tools/issues/23

--- geetools/ui/chart.py
+++ geetools/ui/chart.py
@@ -18,6 +18,75 @@ import pandas as pd
 # TODO: give capability to plot a secondary axis with other data
 
 
+def render_widget(chart, width=None, height=None):
+    """ Render a pygal chart into a Jupyter Notebook """
+    from ipywidgets import HTML
+
+    b64 = base64.b64encode(chart.render()).decode('utf-8')
+
+    src = 'data:image/svg+xml;charset=utf-8;base64,'+b64
+
+    if width and not height:
+        html = '<embed src={} width={}></embed>'.format(src, width)
+    elif height and not width:
+        html = '<embed src={} height={}></embed>'.format(src, height)
+    elif width and height:
+        html = '<embed src={} height={} width={}></embed>'.format(src,
+                                                                  height,
+                                                                  width)
+    else:
+        html = '<embed src={}>'.format(src)
+
+    return HTML(html)
+
+
+def from_pandas(line_chart, dataframe, x=None, y=None, datetime=False):
+    """ Creates a Line chart from a pandas dataFrame """
+    if not isinstance(dataframe, pd.DataFrame):
+        raise ValueError('first argument must be a pandas DataFrame')
+
+    if not x:
+        labels = dataframe[dataframe.columns[0]]
+    else:
+        labels = dataframe[x].values.tolist()
+
+    if not datetime:
+        x_values = labels
+    else:
+        x_values = [tools.date.millis2datetime(d) for d in labels]
+
+    # add property to parsed line_chart object
+    line_chart.x_labels = x_values
+
+    if isinstance(y, list):
+        for column in y:
+            if column == x:
+                continue
+            ydata = dataframe[column].values.tolist()
+            if datetime:
+                # ydata = [(dt, value) for dt, value in zip(x_values, ydata)]
+                # ydata = tuple(zip(x_values, ydata))
+                nydata = []
+                for n, (dt, value) in enumerate(zip(x_values, ydata)):
+                    if not pd.isnull(dataframe[column][n]):
+                        nydata.append((dt, value))
+                ydata = tuple(nydata)
+            else:
+                ydata = [{'value': data} for data in ydata]
+
+            # TODO: add values config
+            # pygal.org/en/latest/documentation/configuration/value.html
+            line_chart.add(column, ydata)
+            line_chart.y_data[column] = ydata
+    else:
+        ydata = dataframe[y].values.tolist()
+        line_chart.add(y, ydata)
+        line_chart.y_data[y] = ydata
+
+    line_chart.data = dataframe
+    return line_chart
+
+
 class Line(pygal.Line):
     def __init__(self, **kwargs):
         super(Line, self).__init__(**kwargs)
@@ -25,63 +94,21 @@ class Line(pygal.Line):
         self.y_data = dict()
         self.x_label_rotation = 30
 
-    @staticmethod
-    def from_pandas(dataframe, x=None, y=None, datetime=False, **kwargs):
-        """ Creates a Line chart from a pandas dataFrame """
-        if not isinstance(dataframe, pd.DataFrame):
-            raise ValueError('first argument must be a pandas DataFrame')
+    def render_widget(self, width=None, height=None):
+        """ Render a pygal chart into a Jupyter Notebook """
+        return render_widget(self, width, height)
 
-        line_chart = Line(**kwargs)
 
-        if not x:
-            labels = dataframe[dataframe.columns[0]]
-        else:
-            labels = dataframe[x].values.tolist()
-
-        if not datetime:
-            x_values = labels
-        else:
-            x_values = [tools.date.millis2datetime(d) for d in labels]
-
-        line_chart.x_labels = x_values
-
-        if isinstance(y, list):
-            for column in y:
-                if column == x:
-                    continue
-                ydata = dataframe[column].values.tolist()
-                ydata = [{'value': data} for data in ydata]
-                # TODO: add values config
-                # pygal.org/en/latest/documentation/configuration/value.html
-                line_chart.add(column, ydata)
-                line_chart.y_data[column] = ydata
-        else:
-            ydata = dataframe[y].values.tolist()
-            line_chart.add(y, ydata)
-            line_chart.y_data[y] = ydata
-
-        line_chart.data = dataframe
-        return line_chart
+class DateTimeLine(pygal.DateTimeLine):
+    def __init__(self, **kwargs):
+        super(DateTimeLine, self).__init__(**kwargs)
+        self.data = None
+        self.y_data = dict()
+        self.x_label_rotation = 30
 
     def render_widget(self, width=None, height=None):
-        from ipywidgets import HTML
-
-        b64 = base64.b64encode(self.render()).decode('utf-8')
-
-        src = 'data:image/svg+xml;charset=utf-8;base64,'+b64
-
-        if width and not height:
-            html = '<embed src={} width={}></embed>'.format(src, width)
-        elif height and not width:
-            html = '<embed src={} height={}></embed>'.format(src, height)
-        elif width and height:
-            html = '<embed src={} height={} width={}></embed>'.format(src,
-                                                                      height,
-                                                                      width)
-        else:
-            html = '<embed src={}>'.format(src)
-
-        return HTML(html)
+        """ Render a pygal chart into a Jupyter Notebook """
+        return render_widget(self, width, height)
 
 
 class Image(object):
@@ -91,7 +118,12 @@ class Image(object):
 
     @staticmethod
     def data2pandas(data):
-        """ Convert data coming from tools.get_values to a pandas DataFrame"""
+        """ Convert data coming from tools.imagecollection.get_values to a
+            pandas DataFrame
+
+            :type data: dict
+            :rtype: pandas.DataFrame
+        """
         # Indices
         # header
         allbands = [val.keys() for bands, val in data.items()]
@@ -142,6 +174,9 @@ class Image(object):
         else:
             ydata = bands
 
+        # Select bands
+        imageCollection = imageCollection.select(ydata)
+
         # Get Images properties
         properties = first.propertyNames().getInfo()
 
@@ -183,11 +218,17 @@ class Image(object):
         df = Image.data2pandas(data)
         newdf = df.sort_values(xProperty)
 
-        line_chart = Line.from_pandas(newdf, y=ydata,
-                                      x=xProperty, datetime=datetime)
+        if datetime:
+            chart = DateTimeLine()
+        else:
+            chart = Line()
+
+        line_chart = from_pandas(chart, newdf, y=ydata,
+                                 x=xProperty, datetime=datetime)
 
         reducer_name = reducer.getInfo()['type'].split('.')[1]
-        chart_title = 'Band {} across images'.format(reducer_name)
+        chart_title = 'Band {} in relation with {}'
+        chart_title = chart_title.format(reducer_name, xProperty)
         line_chart.title = chart_title
 
         return line_chart
@@ -210,15 +251,17 @@ class Image(object):
             # scale = first.select([0]).projection().nominalScale()
             scale = 1
 
-        imageCollection = imageCollection.select([band])
+        # select band
+        imageCollection = imageCollection.select(band)
 
         # Generate data
         # Geometry
         if isinstance(regions, ee.Geometry):
             print('Using `seriesByRegion` with `ee.Geometry` will give you'
                   ' the same output as `series`, use that method instead')
-            chart_title = '{} values in merged geometry across images'.format(
-                                                                          band)
+
+            chart_title = '{} values in merged geometry in relation with {}'
+            chart_title = chart_title.format(band, xProperty)
 
             chart_line = Image.series(imageCollection, regions, reducer,
                                       scale=scale, xProperty=xProperty,
@@ -228,8 +271,12 @@ class Image(object):
 
         elif isinstance(regions, ee.Feature):
             reducer_name = reducer.getInfo()['type'].split('.')[1]
-            chart_title = '{} {} values in one regions across images\nlabeled by {}'.format(
-                band, reducer_name, seriesProperty)
+
+            chart_title = '{} {} values in one regions in relation ' \
+                          'with {}\nlabeled by {}'
+            chart_title = chart_title.format(band, reducer_name,
+                                             xProperty, seriesProperty)
+
             label = regions.get(seriesProperty).getInfo()
             label = label if label else 'unknown feature'
             chart_line = Image.series(imageCollection, regions, reducer,
@@ -269,12 +316,19 @@ class Image(object):
 
             y_labels = newdf.columns.values.tolist()
 
-            line_chart = Line.from_pandas(newdf, y=y_labels,
-                                          x=xProperty, datetime=datetime)
+            if datetime:
+                chart = DateTimeLine()
+            else:
+                chart = Line()
+
+            line_chart = from_pandas(chart, newdf, y=y_labels,
+                                     x=xProperty, datetime=datetime)
 
             reducer_name = reducer.getInfo()['type'].split('.')[1]
-            chart_title = '{} {} values in different regions across images\nlabeled by {}'.format(
-                band, reducer_name, seriesProperty)
+            chart_title = '{} {} values in different regions in relation ' \
+                          'with {}\nlabeled by {}'
+            chart_title = chart_title.format(band, reducer_name,
+                                             xProperty, seriesProperty)
             line_chart.title = chart_title
 
             return line_chart
