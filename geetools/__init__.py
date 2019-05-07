97b0dc8cb811077e77a93893a09e1d02a94a7e5d
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2019-05-07 12:27:12 -0300

    fix Python 2 compatibility

--- geetools/__init__.py
+++ geetools/__init__.py
@@ -13,10 +13,13 @@ __email__ = "fitoprincipe82@gmail.com"
 __license__ = "MIT"
 __copyright__ = "2017 Rodrigo E. Principe"
 
-from . import tools, bitreader, cloud_mask, expressions, decision_tree,\
-              filters, indices
+try:
+    from . import tools, bitreader, cloud_mask, expressions, decision_tree,\
+                  filters, indices
 
-from .ui.ipymap import Map
-from .ui import eprint
-from . import algorithms
-from .tools.imagecollection import wrapper
+    from .ui.ipymap import Map
+    from .ui import eprint
+    from . import algorithms
+    from .tools.imagecollection import wrapper
+except ImportError:
+    pass
