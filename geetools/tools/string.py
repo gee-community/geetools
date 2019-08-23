65e0762d6059ccc50f7fe669c59f9d50f099a206
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2019-08-23 13:35:57 -0300

    format a ee.String similar to str.format

--- geetools/tools/string.py
+++ geetools/tools/string.py
@@ -9,3 +9,27 @@ def eq(string, to_compare):
     string = ee.String(string)
     to_compare = ee.String(to_compare)
     return string.compareTo(to_compare).Not()
+
+
+def format(string, replacement):
+    """ Format a string using variables (as str.format) """
+
+    s = ee.String(string)
+    repl = ee.Dictionary(replacement)
+    keys = repl.keys()
+    values = repl.values()
+    z = keys.zip(values)
+
+    def wrap(kv, ini):
+        keyval = ee.List(kv)
+        ini = ee.String(ini)
+
+        key = ee.String(keyval.get(0))
+        value = ee.String(keyval.get(1))
+
+        pattern = ee.String('{').cat(key).cat(ee.String('}'))
+
+        return ini.replace(pattern, value)
+
+    newstr = z.iterate(wrap, s)
+    return ee.String(newstr)
