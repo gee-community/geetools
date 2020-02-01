9f517805d2048f6621a2bc40f43fa98d53c29144
Rodrigo Principe <rodrigo.principe@agricircle.com>
2020-02-01 11:36:54 -0300

    move authentication function

--- geetools/utils.py
+++ geetools/utils.py
@@ -178,39 +178,3 @@ def formatVisParams(visParams):
             formatted[param] = str(value) if isinstance(value, (int, str)) else ','.join(value)
     return formatted
 
-
-def authenticate(filename, credential_path='default'):
-    """
-    Authenticate to GEE with the specified credentials
-
-    If credential_path is set to 'defualt', it searches for the 'filename' in
-    the same folder in which credentials are stored locally
-    """
-    from google.oauth2.credentials import Credentials
-    import json
-    import os
-
-    if credential_path == 'default':
-        credential_path = os.path.split(ee.oauth.get_credentials_path())[0]
-
-    path = os.path.join(credential_path, filename)
-
-    def get_credentials():
-        try:
-            tokens = json.load(open(path))
-            refresh_token = tokens['refresh_token']
-            return Credentials(
-                None,
-                refresh_token=refresh_token,
-                token_uri=ee.oauth.TOKEN_URI,
-                client_id=ee.oauth.CLIENT_ID,
-                client_secret=ee.oauth.CLIENT_SECRET,
-                scopes=ee.oauth.SCOPES)
-        except IOError:
-            raise ee.EEException(
-                'Please authorize access to your Earth Engine account by '
-                'running\n\nearthengine authenticate\n\nin your command line, and then '
-                'retry.')
-
-    credentials = get_credentials()
-    ee.Initialize(credentials)
