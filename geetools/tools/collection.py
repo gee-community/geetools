f225b03708b0e40cea8c48abd556022ab8588305
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2020-07-30 22:03:59 -0300

    add function to join two collections by a given property

--- geetools/tools/collection.py
+++ geetools/tools/collection.py
@@ -32,3 +32,32 @@ def enumerate(collection):
     list = ee.List(rest.iterate(over_list, start))
 
     return list
+
+
+def joinByProperty(primary, secondary, propertyField):
+    """ Join 2 collections by a given property field.
+    It assumes ids are unique so uses ee.Join.saveFirst.
+    It drops non matching features.
+
+    Example:
+
+    fc1 = ee.FeatureCollection([ee.Feature(geom=ee.Geometry.Point([0,0]),
+                                       opt_properties={'id': 1, 'prop_from_fc1': 'I am from fc1'})])
+    fc2 = ee.FeatureCollection([ee.Feature(geom=ee.Geometry.Point([0,0]),
+                                       opt_properties={'id': 1, 'prop_from_fc2': 'I am from fc2'})])
+    joined = joinById(fc1, fc2, 'id')
+    print(joined.getInfo())
+
+    """
+    Filter = ee.Filter.equals(leftField=propertyField,
+                              rightField=propertyField)
+    join = ee.Join.saveFirst(matchKey='match', outer=False)
+    joined = join.apply(primary, secondary, Filter)
+    def overJoined(feat):
+        properties = feat.propertyNames()
+        retain = properties.remove('match')
+        match = ee.Feature(feat.get('match'))
+        matchprop = match.toDictionary()
+        return feat.select(retain).setMulti(matchprop)
+
+    return joined.map(overJoined)
