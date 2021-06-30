a8b7ca4a9bae1aa3a8d327c2d5b3b2173d843bf0
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2021-06-30 12:37:38 -0300

    v0.6.12

--- geetools/tools/imagecollection.py
+++ geetools/tools/imagecollection.py
@@ -1036,3 +1036,78 @@ def area_under_curve(collection, band, x_property=None, name='area_under'):
     final = ee.List(collection.iterate(cummulative, ee.List([])))
     final_ic = ee.ImageCollection.fromImages(final).select(name)
     return ee.Image(final_ic.reduce(ee.Reducer.sum()))
+
+
+def moving_average(collection, back=5, reducer=ee.Reducer.mean(),
+                   use_original=True):
+    """ Compute the moving average over a time series
+
+    :param back: number of images back to use for computing the stats
+    :type back: int
+    :param reducer: the reducer to apply
+    :type reducer: ee.Reducer
+    :param use_original: if True, computes the stats over the last original
+        values, otherwise, computes the stats over the last computed values
+    :type use_original: bool
+    """
+    def wrap(i, d):
+        d = ee.Dictionary(d)
+        i = ee.Image(i)
+        original = ee.List(d.get('original'))
+        stats = ee.List(d.get('stats'))
+
+        def true(im, di):
+            original_true = ee.List(di.get('original'))
+            stats_true = ee.List(di.get('stats'))
+            original_true = original_true.add(im)
+            tocompute = original_true if use_original else stats_true.add(im)
+            tempcol = ee.ImageCollection.fromImages(tocompute.slice(back * -1))
+            stats = tempcol.reduce(reducer)
+            stats = stats.rename(im.bandNames())
+            stats = stats.copyProperties(im).set('system:time_start',
+                                                 im.date().millis())
+            return ee.Dictionary({
+                'original': original_true,
+                'stats': stats_true.add(stats)
+            })
+
+        def false(im, di):
+            original2 = ee.List(di.get('original'))
+            stats2 = ee.List(di.get('stats'))
+            condition2 = original2.size().gt(0)
+
+            def true2(ima, dic):
+                original_true2 = ee.List(dic.get('original'))
+                original_true2 = original_true2.add(ima)
+                stats_true2 = ee.List(dic.get('stats'))
+                tocompute = original_true2 if use_original else stats_true2.add(
+                    im)
+                tempcol2 = ee.ImageCollection.fromImages(tocompute)
+                stats2 = tempcol2.reduce(reducer)
+                stats2 = stats2.rename(ima.bandNames())
+                stats2 = stats2.copyProperties(ima).set('system:time_start',
+                                                        ima.date().millis())
+                return ee.Dictionary({
+                    'original': original_true2,
+                    'stats': stats_true2.add(stats2)
+                })
+
+            def false2(ima, dic):
+                # first element
+                original_false2 = ee.List(dic.get('original'))
+                stats_false2 = ee.List(dic.get('stats'))
+                return ee.Dictionary({
+                    'original': original_false2.add(ima),
+                    'stats': stats_false2.add(ima)
+                })
+
+            return ee.Dictionary(
+                ee.Algorithms.If(condition2, true2(im, di), false2(im, di)))
+
+        condition = original.size().gte(back)
+        return ee.Dictionary(
+            ee.Algorithms.If(condition, true(i, d), false(i, d)))
+
+    final = ee.Dictionary(
+        collection.iterate(wrap, ee.Dictionary({'original': [], 'stats': []})))
+    return ee.ImageCollection.fromImages(ee.List(final.get('stats')))
