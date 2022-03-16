3cd03ad368bf9642275c6ad977755e169568241e
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2022-03-16 08:58:44 -0300

    add NDFI

--- geetools/indices.py
+++ geetools/indices.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 """ Functions for calculation indices """
+import ee
+
 FORMULAS = {
     'NDVI': '(NIR-RED)/(NIR+RED)',
     'EVI': 'G*((NIR-RED)/(NIR+(C1*RED)-(C2*BLUE)+L))',
@@ -143,8 +145,77 @@ def nbr2(image, swir, swir2, bandname='nbr2'):
     return compute(image, 'NBR2', {'SWIR':swir, 'SWIR2':swir2}, bandname=bandname)
 
 
+def ndfi(image, blue, green, red, nir, swir1, swir2, clouds=0.1,
+         bandname='NDFI'):
+    """ Calculate NDFI using endmembers from Souza et al., 2005.
+    Image values must be float. Returns an Image with the following bands:
+     - GV: Green Vegetation
+     - Shade: Shadow
+     - NPV: Not Photosynthetic Vegetation
+     - Soil: Soil
+     - {bandname}: index name as passed in arguments. Defaults to NDFI, which
+        stands for Normalized Difference Fraction Index
+     """
+    gv = [.0500, .0900, .0400, .6100, .3000, .1000]
+    shade = [0, 0, 0, 0, 0, 0]
+    npv = [.1400, .1700, .2200, .3000, .5500, .3000]
+    soil = [.2000, .3000, .3400, .5800, .6000, .5800]
+    cloud = [.9000, .9600, .8000, .7800, .7200, .6500]
+    cfThreshold = ee.Image.constant(clouds)
+    image = image.select([blue, green, red, nir, swir1, swir2])
+    unmixImage = ee.Image(image).unmix(
+        [gv, shade, npv, soil, cloud], True, True
+    )
+    unmixImage = unmixImage.rename(['band_0', 'band_1', 'band_2',
+                                    'band_3', 'band_4'])
+    newImage = ee.Image(image).addBands(unmixImage)
+    mask = newImage.select('band_4').lt(cfThreshold)
+    ndfi = ee.Image(unmixImage).expression(
+        '((GV / (1 - SHADE)) - (NPV + SOIL)) / ((GV / (1 - SHADE)) + NPV + SOIL)',
+        {
+            'GV': ee.Image(unmixImage).select('band_0'),
+            'SHADE': ee.Image(unmixImage).select('band_1'),
+            'NPV': ee.Image(unmixImage).select('band_2'),
+            'SOIL': ee.Image(unmixImage).select('band_3')
+        })
+
+    final = ee.Image(newImage).addBands(ee.Image(ndfi).rename(['NDFI']))
+    final = final.select(['band_0', 'band_1', 'band_2', 'band_3', 'NDFI'])
+    final = final.rename(['GV', 'Shade', 'NPV', 'Soil', bandname])
+    final = final.updateMask(mask)
+    return final
+
+
+def tasseled_cap_s2(image, blue='B2', green='B3', red='B4', nir='B8',
+                    swir1='B11', swir2='B12'):
+    # Define an Array of Tasseled Cap coefficients.
+    coefficients = ee.Array([
+        [0.3510, 0.3813, 0.3437, 0.7196, 0.2396, 0.1949],
+        [-0.3599, -0.3533, -0.4734, 0.6633, 0.0087, -0.2856],
+        [0.2578, 0.2305, 0.0883, 0.1071, -0.7611, -0.5308]
+    ])
+
+    image = image.select([blue, green, red, nir, swir1, swir2])
+
+    # Make an Array Image, with a 1-D Array per pixel.
+    arrayImage1D = image.toArray()
+
+    # Make an Array Image with a 2-D Array per pixel, 6x1.
+    arrayImage2D = arrayImage1D.toArray(1)
+
+    # Do a matrix multiplication: 6x6 times 6x1.
+    componentsImage = ee.Image(coefficients).matrixMultiply(arrayImage2D)
+
+    # Get rid of the extra dimensions.
+    componentsImage = componentsImage.arrayProject([0]).arrayFlatten(
+    [['brightness', 'greenness', 'wetness']])
+
+    return componentsImage
+
+
 REL = {"NDVI": ndvi,
        "EVI": evi,
        "NBR": nbr,
-       "NBR2": nbr2
+       "NBR2": nbr2,
+       "NDFI": ndfi
        }
