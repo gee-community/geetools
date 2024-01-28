7e33978d4d83908a66abb89bd94899e9eff21dd7
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2024-01-28 17:20:15 +0100

    fix: deprecate vizualization methods

---
+++ tests/test_deprecated.py
@@ -0,0 +1,17 @@
+"""Test all the deprecated methods that have not been kept in the new implementation"""
+
+import pytest
+
+import geetools
+
+class TestVizualisation:
+    """Test methods from the deprecated_visualization module"""
+
+    def test_stretch_std(self):
+        with pytest.raises(NotImplementedError):
+            geetools.visualization.stretch_std(None, None)
+
+    def test_stretch_percentile(self):
+        with pytest.raises(NotImplementedError):
+            geetools.visualization.stretch_percentile(None, None)
+
