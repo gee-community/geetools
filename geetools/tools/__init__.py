4770fe9c5f8bf29bf6302b791678011fdd5d301b
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-10-03 08:30:31 +0000

    refactor: drop deprecated legacy array module

--- geetools/tools/__init__.py
+++ geetools/tools/__init__.py
@@ -3,7 +3,6 @@
 #              computedobject, element, feature
 """Legacy import package for tools."""
 
-from . import _deprecated_array as array  # noqa: F401
 from . import _deprecated_collection as collection  # noqa: F401
 from . import _deprecated_computedobject as computedobject  # noqa: F401
 from . import _deprecated_date as date  # noqa: F401
