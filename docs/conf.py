ac9afe35eae1df07be8f290ef3ee967a2f6f4ff3
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-11-22 11:05:38 +0000

    docs: rewrite gee connection

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,16 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
 # -- Path setup ----------------------------------------------------------------
+import json
 import os
+import re
 import sys
+import tempfile
 from datetime import datetime
 from pathlib import Path
+
+import ee
+import httplib2
 
 # add . to sys to import local extensions
 sys.path.append(str(Path(".").resolve()))
@@ -129,12 +135,30 @@ autosectionlabel_prefix_document = True
 # -- options for myst-nb ------------------------------------------------------
 nb_execution_mode = "force"
 
-
-import pytest_gee  # noqa: E402
-
 if "EARTHENGINE_SERVICE_ACCOUNT" in os.environ:
-    pytest_gee.init_ee_from_service_account()
+
+    # extract the environment variables data
+    private_key = os.environ["EARTHENGINE_SERVICE_ACCOUNT"]
+
+    # small workaround to remove the quotes around the token
+    # related to a very specific issue with readthedocs interface
+    # https://github.com/readthedocs/readthedocs.org/issues/10553
+    pattern = re.compile(r"^'[^']*'$")
+    private_key = private_key[1:-1] if pattern.match(private_key) else private_key
+
+    # connect to GEE using a temp file to avoid writing the key to disk
+    with tempfile.TemporaryDirectory() as temp_dir:
+        file = Path(temp_dir) / "private_key.json"
+        file.write_text(private_key)
+        ee_user = json.loads(private_key)["client_email"]
+        credentials = ee.ServiceAccountCredentials(ee_user, str(file))
+        ee.Initialize(credentials=credentials, http_transport=httplib2.Http())
+
 elif "EARTHENGINE_PROJECT" in os.environ:
-    pytest_gee.init_ee_from_token()
+    # if the user is in local development the authentication should already be available
+    # we simply need to use the provided project name
+    ee.Initialize(project=os.environ["EARTHENGINE_PROJECT"], http_transport=httplib2.Http())
+
 else:
-    raise ValueError("Cannot authenticate with Earth Engine.")
+    msg = "EARTHENGINE_SERVICE_ACCOUNT or EARTHENGINE_PROJECT environment variable is missing"
+    raise ValueError(msg)
