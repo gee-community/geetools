f478dbf743c5d5fa546d50de24939a6728401645
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2025-01-30 10:18:05 +0000

    bump: version 1.11.0 → 1.12.0

--- geetools/__init__.py
+++ geetools/__init__.py
@@ -42,7 +42,7 @@ from .ee_profiler import Profiler
 __title__ = "geetools"
 __summary__ = "A set of useful tools to use with Google Earth Engine Python" "API"
 __uri__ = "http://geetools.readthedocs.io"
-__version__ = "1.11.0"
+__version__ = "1.12.0"
 
 __author__ = "Rodrigo E. Principe"
 __email__ = "fitoprincipe82@gmail.com"
